8457c5230840984e022a69ea849224ec806c9dba
Tim <timothyhollabaugh@gmail.com>
2018-09-18 20:13:59 -0400

    server: Better logging to spreadsheet

--- src/server/server.js
+++ src/server/server.js
@@ -19,6 +19,30 @@ var questions_spreadsheet_id    = get_property("Questions spreadsheet ID");
 var certificate_template_id     = get_property("Certificate template ID");
 var certificate_folder_id       = get_property("Certificate folder ID");
 var config_spreadsheet_id       = get_property("Config spreadsheet ID");
+var log_spreadsheet_id          = get_property("Logs Spreadsheet ID");
+
+var log_sheet = SpreadsheetApp.openById(log_spreadsheet_id).getSheetByName("Current Log");
+var log_id = Utilities.getUuid();
+
+var DEBUG = "DEBUG";
+var INFO = "INFO";
+var WARN = "WARN";
+var ERROR = "ERROR";
+var FATAL = "FATAL";
+
+// Log a message to the log spreadsheet
+// severity: How severe the message is. See constants above
+// context: an object describing any usefull information when the log is called
+// message: the actual log message
+function log(severity, context, message, start) {
+    if (message) {
+        log_sheet.appendRow([new Date(), severity, log_id, JSON.stringify(context), message]);
+    } else {
+        message = context;
+        log_sheet.appendRow([new Date(), severity, log_id, "", message]);
+    }
+    Logger.log(severity + ": " + message);
+}
 
 function setupProperties() {
     properties.setProperties({
@@ -28,22 +52,30 @@ function setupProperties() {
         "Questions spreadsheet ID": "",
         "Certificate template ID": "",
         "Certificate folder ID": "",
+        "Logs Spreadsheet ID": "",
     }, true);
 }
 
 function setupRegistrationForm() {
+    log(INFO, {"f":"setupRegistrationForm"}, "Setting up registration form");
     var registration_form = FormApp.openById(registration_form_id);
     ScriptApp.newTrigger("onRegister").forForm(registration_form).onFormSubmit().create();
 }
 
 function setupEmailTestsForm() {
+    log(INFO, {"f":"setupEmailTestsForm"}, "Setting up email tests form");
     var email_tests_form = FormApp.openById(email_tests_form_id);
     ScriptApp.newTrigger("onEmailTests").forForm(email_tests_form).onFormSubmit().create();
 }
 
 function onRegister(event) {
 
+    var log_context = {'f':'onRegister'};
+
+    log(INFO, {"f":"onRegister", "event":event}, "New registration");
+
     function failure_email(email) {
+        log(WARN, {"f":"failure_email", "email": email}, "Sending failure email");
         GmailApp.sendEmail (
             email,
             "Safety Test Registration Failure",
@@ -68,22 +100,27 @@ function onRegister(event) {
         return info;
 
     }, {class_code: ""});
+    log_context.form_info = form_info;
 
     var email = event.response.getRespondentEmail();
+    log_context.email = email;
 
     var person = PersonLookup.lookupPerson("Email", email)
+    log_context.person = person;
 
     var registration_spreadsheet = SpreadsheetApp.openById(registration_spreadsheet_id);
     var registration_sheet = registration_spreadsheet.getSheetByName(form_info.class_code);
 
     // check that class code exists
     if (registration_sheet == null) {
+        log(WARN, log_context, "Student tried to register with invalid class code");
         failure_email(email);
         return;
     }
 
     // check if class is enabled
     if (!registration_sheet.getRange(2, 4).getValue()) {
+        log(WARN, log_context, "Student tried to register for disabled class code");
         failure_email(email);
         return;
     }
@@ -95,14 +132,16 @@ function onRegister(event) {
 
     if (last_row > 3) {
         var email_column = registration_sheet.getRange(4, 2, last_row-3, 1).getValues();
-
         var email_found = email_column.some(function (current_email) { return current_email == email });
     }
 
     if (email_found) {
+        log(WARN, log_context, "Duplicate email tried to register");
         failure_email(email);
         return;
     }
+
+    log(INFO, log_context, "Successfull registration");
 
     // SUCCESS!
     GmailApp.sendEmail(
@@ -111,6 +150,7 @@ function onRegister(event) {
         "You have been registered to take the safety test\n" +
         "Your instructor will send an email with a link to your test"
     );
+
     registration_sheet.appendRow([
         new Date(),
         email,
@@ -120,6 +160,8 @@ function onRegister(event) {
 
     ]);
 
+    log(DEBUG, log_context, "Sent email and append row for successful registration");
+
 }
 
 function onEmailTests(event) {
@@ -128,6 +170,12 @@ function onEmailTests(event) {
     //   look up name, id
     //   Generate unique url
     //   Send email
+
+    var log_context = {'f': 'onEmailTests'};
+
+    log_context.event = event;
+
+    log(INFO, log_context, "Emailing tests");
 
     var form_items = event.response.getItemResponses();
 
@@ -144,15 +192,15 @@ function onEmailTests(event) {
         return info;
 
     }, {class_code: ""});
-
-    Logger.log(JSON.stringify(form_info));
+    log_context.form_info = form_info;
 
     var registration_spreadsheet = SpreadsheetApp.openById(registration_spreadsheet_id);
     var registration_sheet = registration_spreadsheet.getSheetByName(form_info.class_code);
 
     var student_rows = registration_sheet.getDataRange().getValues().slice(3);
+    log_context.student_rows = student_rows;
 
-    Logger.log(JSON.stringify(student_rows));
+    log(INFO, log_context, "Got info, sending emails");
 
     student_rows.forEach(function (student, index) {
 
@@ -205,7 +253,7 @@ function parseQuestions(questions_spreadsheet) {
                  *  }
                  */
 
-                //Logger.log(row);
+                //log(DEBUG, row);
 
                 return {
                     text: row[0],
@@ -220,7 +268,7 @@ function parseQuestions(questions_spreadsheet) {
                          *  }
                          */
 
-                        //Logger.log(answer + " " + index + " " + row[1] + " " + (index === row[1] - 1));
+                        //log(DEBUG, answer + " " + index + " " + row[1] + " " + (index === row[1] - 1));
 
                         return {
                             text: answer,
@@ -234,14 +282,17 @@ function parseQuestions(questions_spreadsheet) {
 }
 
 function parseConfig(config_sheet) {
-    Logger.log("Parsing config");
-    Logger.log(config_sheet);
-    var config_range = config_sheet.getRange(4, 1, config_sheet.getLastRow()-3, 2);
-    Logger.log("Got range!");
-    Logger.log(config_range);
-    var config_values = config_range.getValues();
+    var log_context = {'f':'parseConfig'};
 
-    Logger.log(JSON.stringify(config_values));
+    log_context.config_sheet = config_sheet;
+
+    log(INFO, log_context, "Parsing config");
+
+    var config_range = config_sheet.getRange(4, 1, config_sheet.getLastRow()-3, 2);
+    var config_values = config_range.getValues();
+    log_context.config_value = config_values;
+
+    log(DEBUG, log_context, "Read config sheet");
 
     var config = config_values.reduce(
         function (config, question) {
@@ -250,14 +301,14 @@ function parseConfig(config_sheet) {
         }, {}
     );
 
-    Logger.log(JSON.stringify(config));
+    log_context.config = config;
+
+    log(DEBUG, log_context, "Parsed config");
 
     return config;
 }
 
 function randomizeQuestions(questions, config) {
-    Logger.log("randomizing questions");
-    Logger.log(JSON.stringify(config));
     var randomized_questions = questions.reduce(function (randomized_questions, category) {
 
         if (category.name in config) {
@@ -283,72 +334,58 @@ function randomizeQuestions(questions, config) {
 }
 
 function submitTest(responses) {
-    Logger.log("Submitted");
+    var log_context = {'f': 'submitTest'};
+    log_context.responses = responses;
 
-    Logger.log(JSON.stringify(responses));
+    log(INFO, log_context, "Test submitted");
 
     var id = parseInt(responses.id);
+    log_context.id = id;
     var class_code = responses.class_code;
+    log_context.class_code = class_code;
     var questions = responses.answers;
+    log_context.questions = questions;
 
     var questions_spreadsheet = SpreadsheetApp.openById(questions_spreadsheet_id);
 
     var total_questions = 0;
     var correct_questions = 0;
 
-    Logger.log(id);
-    Logger.log(class_code);
-    Logger.log(questions);
+    log(INFO, log_context, "Grading test");
 
     questions.forEach(function (question) {
         var category = question.category;
         var id = question.id;
         var response = question.response;
 
-        Logger.log(category);
-        Logger.log(id);
-        Logger.log(response);
-
         var questions_sheet = questions_spreadsheet.getSheetByName(category);
-
         var correct_response = questions_sheet.getRange(4 + id, 2).getValue();
-
-        Logger.log(correct_response);
 
         question.correct_response = correct_response;
 
         var total_count_range = questions_sheet.getRange(4 + id, 8);
         var correct_count_range = questions_sheet.getRange(4 + id, 7);
 
-        Logger.log(total_count_range);
-        Logger.log(correct_count_range);
-
         total_questions += 1;
         total_count_range.setValue(total_count_range.getValue() + 1);
 
         if (response == correct_response) {
-            Logger.log("Correct");
             correct_questions += 1;
             correct_count_range.setValue(correct_count_range.getValue() + 1);
             question.correct = true;
         } else {
-            Logger.log("Not correct");
             question.correct = false;
         }
     });
 
     var response_json = JSON.stringify(responses);
     var score = correct_questions / total_questions;
+    log_context.score = score;
     var passed = score >= 0.8;
-
-    Logger.log(response_json);
-    Logger.log(score);
-    Logger.log(passed);
+    log_context.passed = passed;
 
     var registration_spreadsheet = SpreadsheetApp.openById(registration_spreadsheet_id);
     var registration_sheet = registration_spreadsheet.getSheetByName(class_code);
-
-    Logger.log(id);
 
     registration_sheet.getRange(id + 4, 8).setValue(new Date())
     registration_sheet.getRange(id + 4, 9).setValue(score)
@@ -356,31 +393,35 @@ function submitTest(responses) {
     registration_sheet.getRange(id + 4, 11).setValue(response_json)
 
     var student_data = registration_sheet.getRange(id+4, 1, 1, 7).getValues()[0];
-
-    Logger.log(JSON.stringify(student_data));
+    log_context.student_data = student_data;
 
     var email = student_data[1];
+    log_context.email = email;
     var first_name = student_data[2];
+    log_context.first_name = first_name;
     var last_name = student_data[3];
+    log_context.last_name = last_name;
     var banner_id = student_data[4];
+    log_context.banner_id = banner_id;
 
     if (passed) {
         // Generate certificate and email it
-        Logger.log("passed, generating certificate");
+        log(INFO, log_context, "Passed, generating certificate");
 
         var new_certificate_template_id = registration_sheet.getRange(2, 8).getValue();
+        log_context.new_certificate_template_id = new_certificate_template_id;
 
         if (new_certificate_template_id == "" || new_certificate_template_id == null) {
             new_certificate_template_id = certificate_template_id;
-            Logger.log("no certificate id found, using default");
+            log(WARN, log_context, "no certificate id found, using default");
+            log_context.new_certificate_template_id = new_certificate_template_id;
         }
 
-        Logger.log("using certificate id: " + new_certificate_template_id);
         var certificate_template = DriveApp.getFileById(new_certificate_template_id);
 
         if (certificate_template == null) {
             certificate_template = DriveApp.getFileById(certificate_template_id);
-            Logger.log("could not open certificate template, using default");
+            log(WARN, log_context, "could not open certificate template, using default");
         }
 
         var copyFile = certificate_template.makeCopy();
@@ -404,8 +445,6 @@ function submitTest(responses) {
 
         var date = mm + '/' + dd + '/' + yyyy;
 
-        Logger.log("replacing text");
-
         copyBody.replaceText('<<FirstName>>', first_name);
         copyBody.replaceText('<<LastName>>', last_name);
         copyBody.replaceText('<<BannerID>>', banner_id);
@@ -416,14 +455,10 @@ function submitTest(responses) {
         copyBody.replaceText('<<CompletionDate>>', date);
         copyBody.replaceText('<<CalculatedScore>>', score*100);
 
-        Logger.log("saving cerificate");
-
         copyDoc.saveAndClose();
 
-        Logger.log("generating pdf");
         var pdf = DriveApp.createFile(copyFile.getAs('application/pdf'));
 
-        Logger.log("removing non-pdf");
         copyFile.setTrashed(true);
 
         var folder = DriveApp.getFolderById(certificate_folder_id);
@@ -441,8 +476,7 @@ function submitTest(responses) {
 
         pdf.setName(banner_id + "_" + last_name + "_" + date);
 
-        Logger.log("sending email");
-        Logger.log("email: " + email);
+        log(INFO, log_context, "Sending email");
         GmailApp.sendEmail(
             email,
             "ECE Safety Training Certificate",
@@ -455,8 +489,7 @@ function submitTest(responses) {
             {attachments: [pdf]}
         );
     } else {
-        Logger.log("failed, not generating certificate");
-        Logger.log("email: " + email);
+        log(INFO, log_context, "Failed, not generating certificate");
         GmailApp.sendEmail(
             email,
             "ECE Safety Test",
@@ -468,7 +501,6 @@ function submitTest(responses) {
             "\n" +
             " - The ECE Department"
         );
-        Logger.log("sent email");
     }
 }
 
@@ -483,12 +515,16 @@ function shuffleArray(array) {
 }
 
 function doGet(event) {
-    Logger.log(JSON.stringify(event.parameter));
+    var log_context = {'f':'doGet'};
+    log(INFO, log_context, "Serving request for test");
 
     var id = event.parameter.id;
+    log_context.id = id;
     var class_code = event.parameter.class_code;
+    log_context.class_code = class_code;
 
-    if (id == undefined || id == null || class_code == undefined || id == null) {
+    if (id == undefined || id == null || class_code == undefined || class_code == null) {
+        log(ERROR, log_context, "No ID or no class code in parameters");
         return HtmlService.createHtmlOutput("<p>Invalid Link</p>")
     }
 
@@ -496,33 +532,36 @@ function doGet(event) {
     var registration_sheet = registration_spreadsheet.getSheetByName(class_code);
 
     var row_number = parseInt(id) + 4;
+    log_context.row_number = row_number;
 
     if (registration_sheet == undefined || registration_sheet == null || row_number == NaN || row_number == null || row_number == undefined) {
+        log(ERROR, log_context, "Could not open registration spreadsheet");
         return HtmlService.createHtmlOutput("<p>Invalid Link</p>")
     }
 
     var registration_range = registration_sheet.getRange(row_number, 1, 1, registration_sheet.getLastColumn());
     var registration_row = registration_range.getValues()[0];
+    log_context.registration_row = registration_row;
 
     var config_name = registration_sheet.getRange(2, 6).getValue();
-    Logger.log("config name: " + config_name);
-
-    Logger.log(JSON.stringify(registration_row));
+    log_context.config_name = config_name;
 
     var emailed = registration_row[5];
+    log_context.emailed = emailed;
     var clicked_link = registration_row[6];
+    log_context.clicked_link = clicked_link;
+
+    log(INFO, log_context, "Read registration sheet");
 
     if (emailed == "" || emailed == null || emailed == undefined) {
-        return HtmlService.createHtmlOutput("<p>Invalid Link</p>")
+        log(ERROR, log_context, "Got request from id that was not emailed");
+        return HtmlService.createHtmlOutput("<p>Invalid Link</p>");
     }
 
     if (clicked_link != "") {
-        return HtmlService.createHtmlOutput("<p>This test has already been taken</p>"
-            // + "<pre>" +
-            //clicked_link + "\n" +
-            //row_number + "\n" +
-            //JSON.stringify(registration_row) +
-            //"</pre>"
+        log(WARN, log_context, "Student tried to click link twice");
+        return HtmlService.createHtmlOutput(
+            "<p>This test has already been taken</p>"
         );
     }
 
@@ -535,26 +574,36 @@ function doGet(event) {
     t.config_name = config_name;
 
     var e = t.evaluate();
+
+    log(INFO, log_context, "Sent out test");
+
     return e;
 }
 
 function getQuestions(config_name) {
+    var log_context = {'f':'getQuestions'};
+    log_context.config_name = config_name;
+
+    log(INFO, log_context, "Getting questions");
+
     var questions_sheet = SpreadsheetApp.openById(questions_spreadsheet_id);
     var questions = parseQuestions(questions_sheet);
-
-    Logger.log("Getting questions, config: " + config_name);
+    log_context.questions = questions;
 
     var config_sheet = SpreadsheetApp.openById(config_spreadsheet_id).getSheetByName(config_name);
     var config = parseConfig(config_sheet);
-    Logger.log(JSON.stringify(config));
+    log_context.config = config;
 
     var random_questions = randomizeQuestions(questions, config);
+    log_context.random_questions = random_questions;
 
     random_questions.map(function (question) {
         question.answers.map(function (answer) {
             answer.correct = undefined;
         });
     });
+
+    log(INFO, log_context, "Got questions");
 
     return random_questions;
 }
@@ -565,12 +614,13 @@ function include(filename) {
 }
 
 function tests() {
-    var questions_spreadsheet = SpreadsheetApp.openById(QUESTIONS_SPREADSHEET_ID);
+    var hello = arguments;
+    Logger.log(hello);
+    Logger.log(arguments.callee.name)
+    Logger.log(JSON.stringify(arguments))
+}
 
-    var questions = parseQuestions(questions_spreadsheet);
-    Logger.log(JSON.stringify(questions, null, 2));
-
-    var random_questions = randomizeQuestions(questions);
-    Logger.log(JSON.stringify(random_questions, null, 2));
+function tests2() {
+    tests();
 }
 
